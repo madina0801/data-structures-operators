7ca7b8e41b6ea980c11c00f89515c16f216d897a
Madina <madinatussupova0820@gmail.com>
2022-06-21 13:57:22 +0600

    short-curcuiting && and ||

--- script.js
+++ script.js
@@ -53,9 +53,42 @@ const restaurant = {
   }
 };
 
-// 1. DESTRUCTURING
+// OR Operator
+console.log('~_~ OR ~_~');
 
+// Use ANY data type, return ANY data type, short-circuiting
+console.log(3 || 'Jonas');
+console.log('' || 'Jonas');
+console.log(true || 0);
+console.log(undefined || null);
+
+console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+restaurant.numGuests = 23;
+const quests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+console.log(quests1);
+
+const guests2 = restaurant.numGuests || 10;
+console.log(guests2);
+
+// AND Operator
+console.log('~_~ AND ~_~');
+console.log(0 && 'Jonas');
+console.log(7 && 'Jonas');
+
+console.log('Hello' && 23 && null && 'Jonas');
+
+if(restaurant.orderPizza) {
+  restaurant.orderPizza('mushrooms', 'spinach');
+}
+
+restaurant.orderPizza && restaurant.orderPizza('mushroom', 'spinach');
+
+
+/*
 // REST PATTERN AND PARAMETERS
+
+// 1. DESTRUCTURING
 const arr = [1, 2, ...[3, 4]];
 
 const [a, b, ...others] = [1, 2, 3, 4, 5]
