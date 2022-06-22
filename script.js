46517f964e919ff279f5f48b78c12358593295d8
Madina <madinatussupova0820@gmail.com>
2022-06-22 11:11:20 +0600

    Logical assignment operators

--- script.js
+++ script.js
@@ -53,8 +53,34 @@ const restaurant = {
   }
 };
 
-// Nullish Operator (??)
+const rest1 = {
+  name: 'Capri',
+  // numGuests: 20,
+  numGuests: 0,
+};
 
+const rest2 = {
+  name: 'La Piaza',
+  owner: 'Frederico Rossi',
+};
+
+// rest1.numGuests = rest1.numGuests || 10;
+// rest2.numGuests = rest2.numGuests || 10;
+
+rest1.numGuests ??= 10;
+rest2.numGuests ||= 10;
+
+// rest1.owner = rest1.owner && '<Anonymous>';
+// rest2.owner = rest2.owner && '<Anonymous>';
+
+rest1.owner &&= '<Anonymous>';
+rest2.owner &&= '<Anonymous>';
+
+console.log(rest1);
+console.log(rest2);
+
+// Nullish Operator (??)
+/*
 restaurant.numGuests = 0;
 
 const guests = restaurant.numGuests || 10;
