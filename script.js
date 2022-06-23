864684dee3d55ab0305f799cada4930a4ef297f2
Madina <madinatussupova0820@gmail.com>
2022-06-23 16:24:25 +0600

    entries()

--- script.js
+++ script.js
@@ -58,6 +58,10 @@ const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
 
 for(const item of menu) console.log(item);
 
+for(const [i, el] of menu.entries()) {
+  console.log(`${i + 1}: ${el}`);
+};
+
 /*
 const rest1 = {
   name: 'Capri',
