fb1bf8282d034c483f80d1fe68c98754799eae85
Madina <madinatussupova0820@gmail.com>
2022-06-23 16:08:00 +0600

    for of operator

--- script.js
+++ script.js
@@ -53,6 +53,12 @@ const restaurant = {
   }
 };
 
+
+const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+for(const item of menu) console.log(item);
+
+/*
 const rest1 = {
   name: 'Capri',
   // numGuests: 20,
