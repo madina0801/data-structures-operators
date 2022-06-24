a3fa640d40631ccad2f9a28200187df38f3db7b7
Madina <madinatussupova0820@gmail.com>
2022-06-24 13:54:21 +0600

    Looping Objects

--- script.js
+++ script.js
@@ -56,6 +56,34 @@ const restaurant = {
   },
 };
 
+// LOOPING OBJECTS
+
+// Property Names
+
+const properties = Object.keys(openingHours);
+console.log(properties);
+
+let openStr = `We are open on ${properties.length} days: `;
+
+for(const day of Object.keys(openingHours)) {
+  openStr += `${day},`;
+}
+console.log(openStr);
+
+// Property values
+const values = Object.values(openingHours);
+console.log(values);
+
+// Property entries
+const entries = Object.entries(openingHours);
+console.log(entries);
+
+for(const [key, {open, close}] of entries) {
+  console.log(`On ${key} we open at ${open} and close at ${close}`);
+}
+
+/*
+
 // OPTIONAL CHAINING
 
 // WITHOUT OPTIONAL CHAINING
