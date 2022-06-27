12b7f7291b9890466b1c7a52d4272a0523a5ed52
Madina <madinatussupova0820@gmail.com>
2022-06-27 11:52:38 +0600

    Sets

--- script.js
+++ script.js
@@ -56,9 +56,47 @@ const restaurant = {
   },
 };
 
+// SETS
+
+const ordersSet = new Set([
+  'Pasta',
+  'Pizza',
+  'Pizza',
+  'Risotto',
+  'Pasta',
+  'Pizza',
+]);
+console.log(ordersSet);
+
+console.log(new Set('Jonas'));
+
+console.log(ordersSet.size);
+console.log(ordersSet.has('Pizza'));
+console.log(ordersSet.has('Bread'));
+ordersSet.add('Garlic Bread');
+ordersSet.add('Garlic Bread');
+ordersSet.delete('Risotto');
+// ordersSet.clear();
+
+console.log(ordersSet);
+
+for(const order of ordersSet) console.log(order);
+
+// Example
+const staff = ['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter'];
+
+const staffUnique = [...new Set(staff)];
+console.log(staffUnique);
+console.log(new Set(['Waiter', 'Chef', 'Waiter', 'Manager', 'Chef', 'Waiter']).size);
+
+console.log(new Set('jonasschmedtmann').size);
+
+
 // LOOPING OBJECTS
 
 // Property Names
+
+/*
 
 const properties = Object.keys(openingHours);
 console.log(properties);
